1dfbc9b27e5834cfede762e1b6388b5080b8b72a
Romkashka <halilov.g.2020@gmail.com>
2021-11-24 06:33:16 +0300

    Method print_spruce added

---
+++ forest.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+void print_spruce() {
+    std::cout << "   *   \n";
+    std::cout << "  ***  \n";
+    std::cout << " ***** \n";
+    std::cout << "*******\n";
+    std::cout << "   *   \n";
+}
