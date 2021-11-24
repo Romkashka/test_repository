10f4801c45cf7fac7d756194c43a4c3df10592eb
Romkashka <halilov.g.2020@gmail.com>
2021-11-24 07:26:04 +0300

    Function print_oak added'

--- forest.cpp
+++ forest.cpp
@@ -7,3 +7,13 @@ void print_spruce() {
     std::cout << "*******\n";
     std::cout << "   *   \n";
 }
+
+void print_oak() {
+    std::cout << "  _*_  \n";
+    std::cout << " /***\\ \n";
+    std::cout << "/*****\\\n";
+    std::cout << "|*****|\n";
+    std::cout << "\\_***_/\n";
+    std::cout << "  \\*/  \n";
+    std::cout << "   |   \n";
+}
