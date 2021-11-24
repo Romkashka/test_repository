cdfce77706c0bb923b6fe838d26048f16bb2b453
Romkashka <halilov.g.2020@gmail.com>
2021-11-24 07:02:25 +0300

    Function print_dead_tree added

--- forest.cpp
+++ forest.cpp
@@ -7,3 +7,11 @@ void print_spruce() {
     std::cout << "*******\n";
     std::cout << "   *   \n";
 }
+
+void print_dead_tree() {
+    std::cout << "   |   \n";
+    std::cout << " \\ |\\/ \n";
+    std::cout << "  \\|/   \n";
+    std::cout << "   |/  \n";
+    std::cout << "   |   \n";
+}
